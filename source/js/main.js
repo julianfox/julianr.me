1dde8bc536ddc7f42ae0d4a263a6f2757905b869
Julian <renard.julian@gmail.com>
2016-04-27 15:18:18 +0200

    Improve design

--- source/js/main.js
+++ source/js/main.js
@@ -13,7 +13,7 @@ window.jQuery = window.$ = $;
 $(document).ready(function() {
     var container;
     var camera, controls, scene, renderer, data;
-    var mesh;
+    var texture, mesh;
 
     var worldWidth = 256, worldDepth = 256,
     worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2;
@@ -31,11 +31,11 @@ $(document).ready(function() {
         controls = new THREE.FirstPersonControls( camera );
         controls.lookVertical = false;
         controls.movementSpeed = 50;
-        controls.lookSpeed = 0.005;
+        controls.lookSpeed = 0.010;
         controls.autoForward = true;
 
         scene = new THREE.Scene();
-        scene.fog = new THREE.FogExp2(0xE8E8E8, 0.1);
+        scene.fog = new THREE.FogExp2(0x868E8E, 0.00030);
 
         data = generateHeight( worldWidth, worldDepth );
 
@@ -50,11 +50,18 @@ $(document).ready(function() {
             vertices[ j + 1 ] = data[ i ] * 10;
         }
 
-        mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { wireframe: true } ) );
+        texture = new THREE.CanvasTexture(generateTexture( data, worldWidth, worldDepth ));
+        texture.wrapS = THREE.ClampToEdgeWrapping;
+        texture.wrapT = THREE.ClampToEdgeWrapping;
+
+        mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({
+            map: texture,
+        }));
+
         scene.add(mesh);
 
         renderer = new THREE.WebGLRenderer();
-        renderer.setClearColor( 0xC5CFC6 );
+        renderer.setClearColor( 0x868E8E );
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -71,17 +78,15 @@ $(document).ready(function() {
         controls.handleResize();
     }
 
-    function generateHeight( width, height ) {
+    function generateHeight(width, height) {
         var size = width * height, data = new Uint8Array( size ),
         perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
 
         for ( var j = 0; j < 4; j ++ ) {
 
             for ( var i = 0; i < size; i ++ ) {
-
                 var x = i % width, y = ~~ ( i / width );
                 data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
-
             }
 
             quality *= 5;
@@ -89,7 +94,49 @@ $(document).ready(function() {
         }
 
         return data;
+    }
 
+    function generateTexture(data, width, height) {
+        var canvas, canvasScaled, context, image, imageData,
+        level, diff, vector3;
+        vector3 = new THREE.Vector3( 0, 0, 0 );
+        canvas = document.createElement( 'canvas' );
+        canvas.width = width;
+        canvas.height = height;
+        context = canvas.getContext( '2d' );
+        context.fillStyle = '#000';
+        context.fillRect( 0, 0, width, height );
+        image = context.getImageData( 0, 0, canvas.width, canvas.height );
+        imageData = image.data;
+
+        for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+            vector3.x = data[ j - 2 ] - data[ j + 2 ];
+            vector3.y = 2;
+            vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
+            vector3.normalize();
+            imageData[ i ] = 67 * ( 0.5 + data[ j ] * 0.007 );
+            imageData[ i + 1 ] = 75 * ( 0.5 + data[ j ] * 0.007 );
+            imageData[ i + 2 ] = 78 * ( 0.5 + data[ j ] * 0.007 );
+        }
+
+        context.putImageData( image, 0, 0 );
+        // Scaled 4x
+        canvasScaled = document.createElement( 'canvas' );
+        canvasScaled.width = width * 4;
+        canvasScaled.height = height * 4;
+        context = canvasScaled.getContext( '2d' );
+        context.scale( 4, 4 );
+        context.drawImage( canvas, 0, 0 );
+        image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+        imageData = image.data;
+        for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
+            var v = ~~ ( Math.random() * 5 );
+            imageData[ i ] += v;
+            imageData[ i + 1 ] += v;
+            imageData[ i + 2 ] += v;
+        }
+        context.putImageData( image, 0, 0 );
+        return canvasScaled;
     }
 
     function animate() {
